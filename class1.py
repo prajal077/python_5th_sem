90c0248110c4bd808197e89fc1534014f4e1518a
Prajal <prajalbhattarai1627@gmail.com>
2025-03-16 23:02:27 +0545

    variables

--- class1.py
+++ class1.py
@@ -5,3 +5,11 @@ print("prajal")
 """
     This is multi line comment
 """
+
+#variables 
+
+x = 10  #This is automatically integer
+name = "prajal" #This is automatically string
+bool = "true"
+
+print(x, name, bool)
