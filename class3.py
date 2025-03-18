c878a3951c75fc277b684d3772e28eaed7589105
Prajal <prajalbhattarai1627@gmail.com>
2025-03-18 23:15:22 +0545

    class 3 conditions

---
+++ class3.py
@@ -0,0 +1,12 @@
+#conditions
+if True:
+    print("True")
+else :
+    print("Flase")
+
+#wap to check if the user is above 18 or not
+age = 18
+if(age>18):
+    print("User is above 18")
+else:
+    print("User is below 18")
