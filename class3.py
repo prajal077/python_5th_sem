36d93023625e367eb017979f9b3ec3ec3f8f1b3c
Prajal <prajalbhattarai1627@gmail.com>
2025-03-18 23:30:45 +0545

    checking day using match case

--- class3.py
+++ class3.py
@@ -23,17 +23,38 @@ else :
 
 #wap to check whether the given character is vowel or not
 
-vowel = input("Enter a charecter to check whether it is vowel or nor: ")
-match vowel :
-    case 'a' :
-        print("This is vowel a")
-    case 'e' :
-        print("This is vowel e")
-    case 'i' :
-        print("This is vowel i")
-    case 'o' :
-        print("This is vowel o")
-    case 'u' :
-        print("This is vowel u")
+# vowel = input("Enter a charecter to check whether it is vowel or not: ")
+# match vowel :
+#     case 'a' :
+#         print("This is vowel a")
+#     case 'e' :
+#         print("This is vowel e")
+#     case 'i' :
+#         print("This is vowel i")
+#     case 'o' :
+#         print("This is vowel o")
+#     case 'u' :
+#         print("This is vowel u")
+#     case _:
+#         print("This is not vowel")
+
+#wap using match case to check what day is today
+
+day = int(input("Enter a number to check the day"))
+match day:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("wednesday")
+    case 5:
+        print("Thrusday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
     case _:
-        print("This is not vowel")
+        print("invalid input")
