7a821c02eb2abe107e42321dc10b9fe97f68d592
Prajal <prajalbhattarai1627@gmail.com>
2025-03-19 23:38:33 +0545

    types of parameters in python

--- class4.py
+++ class4.py
@@ -1,46 +1,46 @@
 #functions
 
-def add () :
-    print(5+5)
-add ()
+# def add () :
+#     print(5+5)
+# add ()
 
-def multiply (a, b) :
-    return a*b
+# def multiply (a, b) :
+#     return a*b
 
-a = 10
-b = 20
+# a = 10
+# b = 20
 
-def div () :
-    return b/a
-print(div())
+# def div () :
+#     return b/a
+# print(div())
 
-# parameters in function
+# # parameters in function
 
-def fun (a, b) :
-    print(a+b)
-fun(4, 5)
+# def fun (a, b) :
+#     print(a+b)
+# fun(4, 5)
 
-# even number using function and passing range as a parameter
+# # even number using function and passing range as a parameter
 
-def even (a, b):
-    for i in range (a, b) :
-        if i % 2 == 0 :
-            print(i)
-        else :
-            pass
+# def even (a, b):
+#     for i in range (a, b) :
+#         if i % 2 == 0 :
+#             print(i)
+#         else :
+#             pass
 
-even (0, 11)
+# even (0, 11)
 
-# odd number using function and passing range as a parameter
+# # odd number using function and passing range as a parameter
 
-def odd (a, b):
-    for i in range (a, b) :
-        if i % 2 != 0 :
-            print(i)
-        else :
-            pass
+# def odd (a, b):
+#     for i in range (a, b) :
+#         if i % 2 != 0 :
+#             print(i)
+#         else :
+#             pass
 
-odd (0, 11)
+# odd (0, 11)
 
 #function types
 # 1. no parameter with no return type
@@ -50,3 +50,4 @@ def name (a, b) :
     print(a+b)
 
 name("prajal", " bhattarai")
+
