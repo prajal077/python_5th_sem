7970f666c7a61fb6ed7a9394bb086879fbb8a323
Prajal <prajalbhattarai1627@gmail.com>
2025-03-21 00:02:10 +0545

    final commit, finished the given task

--- class5.py
+++ class5.py
@@ -56,3 +56,28 @@ class Two :
 
 Two.cdf(5, 5)
 
+#wap to make class named STUDENTS with the attributes need to make the constructor with values name, age, gender, and batch.
+# make 3 methods where the 1st method give the sum of 2 number, 2nd method print the name, age and the last method print gender and batch
+
+class Students :
+    def __init__(self, name, age, gender, batch):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.batch = batch
+
+    def add (self, a, b) :
+            print(a+b)
+
+    def second (self) :
+        print(f"name = {self.name}, age = {self.age}")
+
+    def third (self) :
+        print(f"gender = {self.gender}, batch = {self.batch}")
+
+
+student1 = Students("Prajal", 20, "Male", "Fifth")
+student1.add(3, 3)
+student1.second()
+student1.third()
+            
