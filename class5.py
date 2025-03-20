5add0496c9530bf3380cd830fa25a2a3abebfbbe
Prajal <prajalbhattarai1627@gmail.com>
2025-03-20 23:01:36 +0545

    changing the content of class through object

--- class5.py
+++ class5.py
@@ -11,3 +11,7 @@ class Colony :
 house1 = Colony()
 print(house1.house)
 print(house1.name)
+
+house2 = Colony()
+house2.name = "ram"
+print(house2.name)    #changing the values of class through  object
