66ee7a3b7e51efb5b62469ed7b5b5c620367bf53
Prajal <prajalbhattarai1627@gmail.com>
2025-03-21 21:24:50 +0545

    first example of encapsulatoin

--- class6.py
+++ class6.py
@@ -1,5 +1,22 @@
 # encapsulation
 
 class Encap :
-    def __init__(self, a, b):
+    def __init__(self, a, b, c):
+        self.a = a      #public
+        self._b = b     #protected
+        self.__c = c     #private
+
+    def getInfo (self) :
+        print(f"a = {self.a}, b = {self._b}, c = {self.__c}")
         
+    #changing the value in getInfo function
+    def change(self, c):
+        self.__c = c 
+            
+obj = Encap(10, 20, 30)
+obj.getInfo()
+
+print(obj.a)
+print(obj._b)
+# print(obj.__c)    This cause error cause we cannot access the private variable from outside the function
+
